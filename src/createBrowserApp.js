d5685460db3e4f07c4b678a617ca7bfb2ccd5050
Jonathan Limtiaco <jonathanlimtiaco@gmail.com>
2019-11-06 17:10:00 -0800

    Update dep

--- src/createBrowserApp.js
+++ src/createBrowserApp.js
@@ -14,7 +14,7 @@ import {
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 /* eslint-disable import/no-commonjs */
-const queryString = require('query-string');
+const queryString = require('qs');
 
 const getPathAndParamsFromLocation = location => {
   const path = encodeURI(location.pathname.substr(1));
